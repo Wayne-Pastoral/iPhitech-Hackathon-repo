72b0f02dc880ab2b6b056d4acb5b44e1c6bdcd13
jeanestrera <jeanestrera0000@gmail.com>
2021-10-02 14:44:37 +0800

    dashboard
    dashboard

--- Index.php
+++ Index.php
@@ -8,14 +8,166 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>The Hawks</title>
 </head>
 
 <body>
+    <div id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <nav class="navbar navbar-light">
+                        <a class="navbar-brand" href="#">
+                            <img src="/assets/image/logo.png" width="35%" height="auto" alt="">
+                        </a>
+                    </nav>
+                </div>
+                <div class="col-6 menu">
+                    <nav class="navbar navbar-light">
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-3 login">
+                    <nav class="navbar navbar-light">
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item login">
+                                <a class="nav-link" href="#"> <i class="fas fa-user"></i>Login</a>
+                            </li>
+                            <li class="nav-item login">
+                                <a class="nav-link disabled" href="#">Sign Up</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<h2>dsadasd</h2>
-       
-<footer>
+    <div id="banner-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h1 class="">
+                        WE ARE THE SEAFARER'S INTERNATIONAL
+                    </h1>
+                    <h3>
+                        US Maritime Unions Playing Essential role in the Covid-19 Fight
+                    </h3>
+                </div>
+                <div class="hero-img col-6">
+                    <img src="https://members.seafarers.org/images/img1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="dashboard">
+        <div class="container">
+            <h2 class="member">MEMBERS DASHBOARD</h2>
+            <div class="row">
+                <div class="col">
+                    <i class="fas fa-user-edit"></i>
+                    <h6>First, but unordered</h6>
+                    <button>ADD MEMBERS</button>
+                </div>
+                <div class="col">
+                    <i class="fas fa-user-tag"></i>
+                    <h6>Second, but last</h6>
+                    <button>ADD ASSIGNED MEMBERS</button>
+                </div>
+                <div class="col">
+                    <i class="fas fa-users-cog"></i>
+                    <h6>Third, but first</h6>
+                    <button>ADD MEMBERS</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="table">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="partners">
+        <div class="container">
+            <h4 class="partners">
+                We are grateful to our main funders for their continued support:
+            </h4>
+            <div class="row">
+                <div class="col-4">
+                    <img src="https://www.seafarerhelp.org/assets/images/sponsors/_150x100_fit_top-center_95/seafarers-trust-logo-horizontal.png"
+                        alt="">
+                </div>
+                <div class="col-4">
+                    <img src="https://www.seafarerhelp.org/assets/images/sponsors/_150x100_fit_top-center_95/The-Seafarers-Charity-2.png"
+                        alt="">
+                </div>
+                <div class="col-4">
+                    <img src="https://www.seafarerhelp.org/assets/images/sponsors/_150x100_fit_top-center_95/The-TK-Foundation.png"
+                        alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <footer>
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        © 2021 Seafarers’ International. Registered charity number: 123456
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
@@ -25,7 +177,6 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-</footer>
 </body>
 
 </html>
